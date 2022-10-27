d8d9bbe4eeebc497a9e8948194ad190361ffda10
Bhanuka Uyanage <50510335+BhanukaC@users.noreply.github.com>
2022-10-27 23:46:59 +0530

    Initial version

---
+++ Plant-Distribution Backend/src/test/java/com/example/Plant/Distribution/PlantDistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Plant.Distribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantDistributionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
