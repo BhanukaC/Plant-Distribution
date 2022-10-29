cb33a6263e6669ef7b401e009bbd71f9d7c31f11
Bhanuka Uyanage <50510335+BhanukaC@users.noreply.github.com>
2022-10-29 07:39:06 +0530

    Update PlantController.java

--- Plant-Distribution Backend/src/main/java/com/example/Plant/Distribution/controller/PlantController.java
+++ Plant-Distribution Backend/src/main/java/com/example/Plant/Distribution/controller/PlantController.java
@@ -1,4 +1,15 @@
 package com.example.Plant.Distribution.controller;
 
+import com.example.Plant.Distribution.model.Plant;
+import com.example.Plant.Distribution.repository.PlantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/plant")
 public class PlantController {
+
+    @Autowired
+    private PlantRepository plantRepository;
 }
